8ccf493189a722a4148d243c464e3c594fe22d57
50Bytes <50Bytes.dev@gmail.com>
2021-07-22 22:01:45 +0500

    try async

--- twint/run.py
+++ twint/run.py
@@ -329,7 +329,7 @@ def run(config, callback=None):
     get_event_loop().run_until_complete(Twint(config).main(callback))
 
 
-def run_async(config, callback=None):
+async def run_async(config, callback=None):
     logme.debug(__name__ + ':run_async')
     await Twint(config).main(callback)
 
@@ -389,6 +389,19 @@ def Lookup(config):
     config.Followers = False
     config.TwitterSearch = False
     run(config)
+    if config.Pandas_au:
+        storage.panda._autoget("user")
+
+
+def LookupAsync(config):
+    logme.debug(__name__ + ':Lookup')
+    config.Profile = False
+    config.Lookup = True
+    config.Favorites = False
+    config.FOllowing = False
+    config.Followers = False
+    config.TwitterSearch = False
+    await run_async(config)
     if config.Pandas_au:
         storage.panda._autoget("user")
 
